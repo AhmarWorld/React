940aeee917d9a388ddbf3dc9622ef946da68802e
Ahmar <ahmarkakimov2005@gmail.com>
2023-08-15 20:00:55 +0600

    Lesson 1th files

---
+++ lesson 1/react-beginners-projects-counter/src/components/Counter.js
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import '../App.css'
+
+export default function Counter() {
+    const [count, setCount] = useState(0)
+
+    const onPlus = () => {
+        const newCount = count + 1
+        setCount(newCount)
+    }
+
+    const onMinus = () => {
+        const newCount = count - 1
+        setCount(newCount)
+    }
+    return (
+        <div className="counter__container">
+            <h2>
+                Счетчик:
+            </h2>
+            <h1>
+                {count}
+            </h1>
+            <div>
+                <button 
+                 onClick={onMinus}
+                 className="counter_button minus">- Минус</button>
+                <button 
+                onClick={onPlus}
+                className="counter_button plus">Плюс +</button>
+            </div>
+        </div>
+    )
+}
---
+++ lesson 1/todo-list/src/components/Card.js
@@ -0,0 +1,7 @@
+import './App.css';
+
+export default function Card (){
+    return(
+        <h1>Get Things Done</h1>
+    )
+}
