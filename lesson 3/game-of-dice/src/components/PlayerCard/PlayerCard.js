b9c1fda293f8bc547a0eec8635e7aa3015299a4b
Ahmar <ahmarkakimov2005@gmail.com>
2023-08-23 19:10:34 +0600

    fixes

--- lesson 3/game-of-dice/src/components/PlayerCard/PlayerCard.js
+++ lesson 3/game-of-dice/src/components/PlayerCard/PlayerCard.js
@@ -1,16 +1,20 @@
-import './PlayerCard.css'
+import "./PlayerCard.css";
 
-export default function PlayerCard({name,score,currentPoints, isCurrent}){
-    return(
-        <div className='player-card' id={`${isCurrent && 'current-player'}`}>
-            <div className='player-card_head'>
-                <h1>{name}</h1>
-                <p>{score}</p>
-            </div>
-            <div className='palyer-card_foot'>
-                <span style={{fontSize:'16px', color:'#EDDAC6FF'}}>CURRENT POINTS</span>
-                <span style={{fontSize:'20px', }}>{currentPoints}</span>
-            </div>
-        </div>
-    )
+export default function PlayerCard({ name, score, currentPoints, isCurrent }) {
+  return (
+    <div className="player-card" id={`${isCurrent && "current-player"}`}>
+      <div className="player-card_head">
+        <h1>{name}</h1>
+        <p>{score}</p>
+      </div>
+      <div className="palyer-card_foot">
+        <span style={{ fontSize: "16px", color: "#EDDAC6FF" }}>
+          CURRENT POINTS
+        </span>
+        <span style={{ fontSize: "20px", color: "white" }}>
+          {currentPoints}
+        </span>
+      </div>
+    </div>
+  );
 }
