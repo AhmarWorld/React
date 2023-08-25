7715c269cec13b6cea79b6892dfbcd7206ee9891
Ahmar <ahmarkakimov2005@gmail.com>
2023-08-26 00:24:01 +0600

    Create app weather

---
+++ lesson 3/weather/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+const API = {
+  key:'7f7a019a2371f95afe97771c376705b7',
+  base:'https://api.openweathermap.org/data/2.5/'
+}
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
