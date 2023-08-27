d58887b8936c71fe5a728ab365e246906b249f24
Ahmar <ahmarkakimov2005@gmail.com>
2023-08-28 04:31:38 +0600

    New design, completed app

--- lesson 4/recipes-app/src/components/RecipeCard/RecipeCard.js
+++ lesson 4/recipes-app/src/components/RecipeCard/RecipeCard.js
@@ -1,16 +1,20 @@
 import './RecipeCard.css';
 
-export default function Recipe({title,calories,image,ingredients}){
-    return(
-        <div className={`food-card`}>
-            <h1>{title}</h1>
-            <ol>
-                {ingredients.map(ingredient=>(
-                    <li>{ingredient.text}</li>
-                ))}
-            </ol>
-            <p>Calories: {calories.toFixed()}</p>
-            <img className='food-img' src={image} alt=":/" />
+export default function Recipe({ title, calories, image, ingredients }) {
+    return (
+        <div className='food-card'>
+            <div className='food-avatar'>
+                <img className='food-img' src={image} alt=":/" />
+                <p>Calories: {calories.toFixed()}</p>
+            </div>
+            <div className='food-receip'>
+                <h1>{title}</h1>
+                <ol>
+                    {ingredients.map(ingredient => (
+                        <li>{ingredient.text}</li>
+                    ))}
+                </ol>
+            </div>
         </div>
     )
 }
--- lesson 4/recipes-app/src/components/SearchForm/SearchForm.js
+++ lesson 4/recipes-app/src/components/SearchForm/SearchForm.js
@@ -1,10 +1,17 @@
 import './SearchForm.css';
+import { useState } from 'react';
 
-export default function SearchForm({value, setSearch, onSubmit}) {
+export default function SearchForm({ value, setSearch, onSubmit }) {
+    const [buttonDisabled, setButtonDisabled] = useState(true)
+
     return (
         <form onSubmit={onSubmit} className="search-form">
-            <input type="text" className="search-bar" value={value} onChange={(e) => { setSearch(e.target.value) }} placeholder='Food which is recipe do you want?'/>
-            <button className="search-button">Search</button>
+            <input type="text" className="search-bar" value={value} onChange={(e) => {
+                setButtonDisabled(e.target.value ? false : true)
+                setSearch(e.target.value)
+            }}
+                placeholder='Food which is recipe do you want?' />
+            <button className="search-button" disabled={buttonDisabled}>Search</button>
         </form>
     )
 }
