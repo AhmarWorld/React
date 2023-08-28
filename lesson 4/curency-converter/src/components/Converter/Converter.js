f910c432b7b2457727772e37e1eca5627844cbfa
Ahmar <ahmarkakimov2005@gmail.com>
2023-08-29 01:31:51 +0600

    Completed Curency-Converter

--- lesson 4/curency-converter/src/components/Converter/Converter.js
+++ lesson 4/curency-converter/src/components/Converter/Converter.js
@@ -1,6 +1,7 @@
 import "./Converter.css";
 import ConvertArea from "../ConvertArea/ConvertArea";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { fetchCourseData } from '../../lib/fetchCourseData';
 
 export default function Converter() {
   const [inputValueOne, setInputValueOne] = useState(1);
@@ -8,6 +9,22 @@ export default function Converter() {
   const curencyArray = ["KZT", "USD", "EUR", "GBP"];
   const [activeButtonOne, setActiveButtonOne] = useState(curencyArray[0]);
   const [activeButtonTwo, setActiveButtonTwo] = useState(curencyArray[0]);
+  const [curencyData, setCurencyData] = useState({})
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const courseData = await fetchCourseData();
+      setCurencyData(courseData.rates);
+      console.log(courseData.rates);
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const value = inputValueOne*(curencyData[activeButtonTwo] / curencyData[activeButtonOne])
+    setInputValueTwo(Math.round(value*100)/100)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [inputValueOne, activeButtonOne, activeButtonTwo])
 
   return (
     <div className="main">
