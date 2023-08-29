164445a9aa39dadc89685aa39c4390c0f8360d51
Ahmar <ahmarkakimov2005@gmail.com>
2023-08-29 21:49:05 +0600

    Chakra added for converter

--- lesson 4/curency-converter/src/App.js
+++ lesson 4/curency-converter/src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import Converter from "./components/Converter/Converter";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <Converter />
-    </div>
+    <ChakraProvider>
+      <div className="App">
+        <Converter />
+      </div>
+    </ChakraProvider>
   );
 }
 
