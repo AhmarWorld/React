a1755e01f9f4f6958a8222c26917dcf23d52d0dc
Ahmar <ahmarkakimov2005@gmail.com>
2023-08-31 10:33:16 +0600

    Completed contacts-card app

--- lesson 5/contact-list/src/App.js
+++ lesson 5/contact-list/src/App.js
@@ -1,8 +1,30 @@
 import './App.css';
+import ContactCard from './components/ContactCard/ContactCard';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [contacts, setContacts] = useState([])
+
+  useEffect(()=>{
+    fetch('https://randomuser.me/api/?results=10')
+    .then(response => response.json())
+    .then(({ results }) => setContacts(results))
+  }, [])
+
   return (
     <div className="App">
+      {contacts.map((contact, index) => {
+        return (
+          <ContactCard
+            key={index}
+            avatarUrl={contact.picture.medium}
+            name={`${contact.name.title} ${contact.name.first} ${contact.name.last}`}
+            email={contact.email}
+            age={contact.dob.age}
+          />
+        )
+      })}
     </div>
   );
 }
